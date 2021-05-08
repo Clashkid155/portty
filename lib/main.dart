55b56b5cbb9279557a7abf4979422e4d5958eb1c
Clashkid155 <frankdiss9@gmail.com>
2021-05-08 11:43:17 +0100

    App redesigned

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Steam Portfolio',
       home: Portty(),
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+          fontFamily: 'Ltim',
+          textTheme: TextTheme(
+            bodyText2: TextStyle(fontSize: 16),
+            bodyText1: TextStyle(fontSize: 16),
+            //: TextStyle(fontSize: 16),
+          ),
+          brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -35,10 +42,8 @@ class _PorttyState extends State<Portty> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
-      setState(() {
-        _visible = false;
-      });
+    Future.delayed(const Duration(seconds: 4), () {
+      setState(() => _visible = false);
     });
   }
 
@@ -46,6 +51,7 @@ class _PorttyState extends State<Portty> {
   Widget build(BuildContext context) {
     var x = [
       Colors.red,
+      Colors.cyan,
       Colors.blue,
       Colors.green,
       Colors.orange,
@@ -53,39 +59,48 @@ class _PorttyState extends State<Portty> {
       Colors.purple,
     ];
     Future.delayed(Duration(seconds: 8), () {
-      print(i);
-
-      setState(() {
-        i >= 5 ? i = 0 : i++;
-      });
+      setState(() => i >= 5 ? i = 0 : i++);
     });
 
     Size mid = MediaQuery.of(context).size;
     return SafeArea(
-      child: Scaffold(
-          body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          children: [
-            SizedBox(
-              height: mid.height * 0.1,
-            ),
-            Stack(children: [
+        child: Scaffold(
+      /* appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Text('Hi'),
+        actions: [
+          Icon(Icons.ac_unit),
+          SizedBox(
+            width: 15,
+          )
+        ],
+      ),*/
+      body: Container(
+        color: Colors.black54,
+        height: mid.height,
+        width: mid.width,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               GestureDetector(
                 child: Center(
-                  child: Container(
+                  child: AnimatedContainer(
+                    curve: Curves.bounceInOut,
+                    duration: Duration(seconds: 3),
                     decoration:
                         BoxDecoration(shape: BoxShape.circle, boxShadow: [
                       BoxShadow(
                         color: x[i],
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 2),
+                        spreadRadius: 6,
+                        blurRadius: 4,
+                        offset: Offset(0, 1),
                       )
                     ]),
                     child: CircleAvatar(
                       backgroundImage: AssetImage(check ? bitm : steam),
-                      radius: 100,
+                      radius: 65,
                     ),
                   ),
                 ),
@@ -93,110 +108,140 @@ class _PorttyState extends State<Portty> {
                   setState(() => !check ? check = true : check = false);
                 },
               ),
-              Visibility(
-                visible: _visible,
-                child: AnimatedPositioned(
-                  curve: Curves.easeInOut,
-                  duration: Duration(seconds: 4),
-                  top: mid.width * 0.45, //168,
-                  right: mid.width * 0.01,
-                  child: Text('Tap image to change'),
+              SizedBox(
+                height: 15,
+              ),
+              AnimatedOpacity(
+                opacity: _visible ? 1 : 0,
+                duration: Duration(seconds: 3),
+                curve: Curves.easeInOutCubic,
+                child: Text('Tap image to change.'),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  height: mid.height * 0.5,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          spreadRadius: 10,
+                          blurRadius: 10,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Column(children: [
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Hi there, I\'m ',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          'Beluchukwu.',
+                          style: TextStyle(fontSize: 25, fontFamily: 'Caveat'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Flexible(
+                      child: Text(
+                        'I\'m an aspiring mobile developer who is intrested in not just developing apps but also how the system works. I\'m also intrested in circuit programming and blockchain technology.',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Contact',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ListTile(
+                      leading: ImageIcon(
+                        AssetImage(git),
+                        color: Colors.white,
+                      ),
+                      title: SelectableText(
+                        ' @Clashkid155',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                    ),
+                    ListTile(
+                      leading: ImageIcon(
+                        AssetImage(twit),
+                        color: Colors.blue,
+                      ),
+                      title: SelectableText(
+                        ' @x_steamer',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Skills/Stacks',
+                          style: TextStyle(fontSize: 18),
+                        )),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        ImageIcon(
+                          AssetImage(py),
+                        ),
+                        Card(
+                            elevation: 5,
+                            child: Text(
+                              ' Python ',
+                              style: TextStyle(fontSize: 18),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ImageIcon(
+                          AssetImage(flut),
+                          //color: Colors.red,
+                        ),
+                        Card(
+                            elevation: 5,
+                            child: Text(
+                              ' Flutter ',
+                              style: TextStyle(fontSize: 18),
+                            )),
+                        SizedBox(
+                          width: 5,
+                        ),
+                      ],
+                    ),
+                  ]),
                 ),
               ),
-            ]),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Hi! I\'m Beluchukwu',
-              style: TextStyle(fontSize: 20),
-            ),
-            Flexible(
-              child: Text(
-                'I\'m an aspiring mobile developer who is intrested in not just developing apps but also how the system works. I\'m also intrested in circuit programming and blockchain technology.',
-                style: TextStyle(fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Contact',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              // tileColor: Colors.grey.shade100,
-              leading: ImageIcon(
-                AssetImage(git),
-                color: Colors.white,
-              ),
-              title: SelectableText(
-                ' @Clashkid155',
-                // style: TextStyle(color: Colors.indigo),
-              ),
-            ),
-            ListTile(
-              //tileColor: Colors.grey.shade100,
-              leading: ImageIcon(
-                AssetImage(twit),
-                color: Colors.blue,
-              ),
-              title: SelectableText(
-                ' @x_steamer',
-                //style: TextStyle(color: Colors.indigo),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Skills/Stacks',
-                  style: TextStyle(fontSize: 18),
-                )),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                ImageIcon(
-                  AssetImage(py),
-                ),
-                Card(
-                    elevation: 5,
-                    child: Text(
-                      'Python',
-                      style: TextStyle(fontSize: 18),
-                    )),
-                SizedBox(
-                  width: 10,
-                ),
-                ImageIcon(
-                  AssetImage(flut),
-                  //color: Colors.red,
-                ),
-                Card(
-                    elevation: 5,
-                    child: Text(
-                      'Flutter',
-                      style: TextStyle(fontSize: 18),
-                    )),
-                SizedBox(
-                  width: 5,
-                ),
-                //Icon
-              ],
-            )
-          ],
+            ],
+          ),
         ),
-      )),
-    );
+      ),
+    ));
   }
 }
